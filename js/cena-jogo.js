62bf8e5c223c202bdd4aafc765565b0db5dbae6d
JVitoor <56413811+JVitoor@users.noreply.github.com>
2020-07-23 13:15:03 -0300

    Update cena-jogo.js

--- js/cena-jogo.js
+++ js/cena-jogo.js
@@ -22,9 +22,69 @@ export default class CenaJogo extends Phaser.Scene {
         plataformas.create(0, 184, 'chao').setOrigin(0, 0).refreshBody();
         plataformas.create(400 - 30, 240 - 56 - 34 - 34, 'platform').setOrigin(0, 0).refreshBody();
         plataformas.create(400 - 60, 240 - 56 - 34, 'platform').setOrigin(0, 0).refreshBody();
+        plataformas.create(190, 120, 'platform').setOrigin(0, 0).refreshBody();
+        plataformas.create(30, 80, 'platform').setOrigin(0, 0).refreshBody();
 
         this.jogador = new Jogador(this);
         this.physics.add.collider(this.jogador.sprite, plataformas);
+        const jogador = this.jogador.sprite;
+
+        const estrelas = this.physics.add.group();
+        estrelas.createFromConfig({
+            key: "estrela",
+            repeat: 4,
+            setXY: { x: 60, y: 0, stepX: 80 },
+            setScale: { x: 1, y: 1 },
+          });
+      
+          estrelas.children.iterate((estrela) => {
+            estrela.setBounceY(Phaser.Math.FloatBetween(0.2, 0.6));
+          });
+      
+          this.physics.add.collider(estrelas, plataformas);
+          this.physics.add.overlap(this.jogador.sprite, estrelas, coletaEstrela, null, this);
+
+        let pontos = 0;
+        let textoPontos;
+            textoPontos = this.add.text(10, 200, "Pontuação: 0", { fontSize: '32px', fill: '#000' });
+
+        function coletaEstrela (jogador, estrela){
+            estrela.disableBody(true, true);
+
+            pontos += 10;
+            textoPontos.setText(`Pontuação: ` + pontos);
+
+            if (estrelas.countActive(true) === 0){
+                estrelas.children.iterate(function (child) {
+
+                child.enableBody(true, child.x, 0, true, true);
+
+                });
+
+                let x = (this.jogador.sprite.x < 200) ? Phaser.Math.Between(200, 400) : Phaser.Math.Between(0, 200);
+
+                let inimigo = inimigos.create(x, 16, 'inimigo');
+                inimigo.setBounce(1);
+                inimigo.setCollideWorldBounds(true);
+                inimigo.setVelocity(Phaser.Math.Between(-200, 200), 20);
+            }
+        }
+
+        const inimigos = this.physics.add.group();
+        let gameOver;
+
+        this.physics.add.collider(inimigos, plataformas);
+        this.physics.add.collider(this.jogador.sprite, inimigos, colideInimigo, null, this);
+
+        function colideInimigo(){
+            this.physics.pause();
+
+            jogador.setTint(0xff0000);
+
+            jogador.anims.play('turn');
+
+            gameOver = true;
+        }
 
         this.teclas = this.input.keyboard.createCursorKeys();
     }
